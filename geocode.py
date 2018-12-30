5e425563656129a6b6b8591dcead8573e5aff9d8
Joose Helle <joose.helle@helsinki.fi>
2018-12-30 22:10:36 +0200

    Implement simple geocoder & command line interface for it

---
+++ geocode.py
@@ -0,0 +1,35 @@
+import requests
+import json
+from urllib.parse import urlparse, urlencode
+
+def getGeocodeRequest(search_word):
+    print('\nSearch word:', search_word)
+    vars = {'text': search_word, 'size': 1}
+    baseurl = 'https://api.digitransit.fi/geocoding/v1/search?'
+    request = baseurl + urlencode(vars)
+    print('request:', request)
+    return request
+
+def geocode(search_word):
+    # build request url
+    request = getGeocodeRequest(search_word)
+    # execute API call
+    georeq = requests.get(request)
+    # print('JSON:', georeq.text)
+    
+    # parse result from json to python dictionary
+    parsed_result = json.loads(georeq.text)
+        
+    # extract geocoded feature (GeoJSON)
+    feat = parsed_result['features'][0]
+    geom = feat['geometry']
+    props = feat['properties']
+    # print(geom)
+    # print(props)
+    
+    print('found:', props['label'])
+    print('at:', geom['coordinates'])
+    print('from neighbourhood:', props['neighbourhood'])
+    print('with confidence:', props['confidence'])
+    
+    return geom['coordinates']
---
+++ geocoder.py
@@ -0,0 +1,12 @@
+import geocode as gc
+
+print('\nStarting geocoder')
+
+while True:
+    print('\nWrite the search word or address to geocode...')
+    search_word = input()
+    
+    if(len(search_word)==0):
+        break
+        
+    coords = gc.geocode(search_word)
