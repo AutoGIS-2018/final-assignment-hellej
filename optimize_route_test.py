c2550bab6c7d64ef1f70ed0e2356012383ef025f
Joose Helle <joose.helle@helsinki.fi>
2019-01-02 20:24:47 +0200

    Refactor

--- optimize_route_test.py
+++ optimize_route_test.py
@@ -11,7 +11,8 @@ coords = gc.geocode(search_word)
 # GET TRAVEL TIMES
 #%%
 import geopandas as gpd
-import utils.matrix as mu
+import utils.matrix as mtrx
+import utils.routes_tt as rtts
 
 #%%
 cinemas = gpd.read_file('data/temp/cinemas.shp')
@@ -19,24 +20,24 @@ large_cinemas = cinemas.loc[cinemas['rooms'] > 2]
 target_points = large_cinemas[:4]
 
 #%%
-target_info = mu.targets_ykr_ids(target_points, 'name')
+target_info = mtrx.targets_ykr_ids(target_points, 'name')
 
 #%%
-tt_dfs = mu.get_tt_between_targets(target_info, 'data/HelsinkiTravelTimeMatrix2018/')
+tt_dfs = mtrx.get_tt_between_targets(target_info, 'data/HelsinkiTravelTimeMatrix2018/')
 
 #%%
-target_perms = mu.get_target_permutations(tt_dfs)
+target_perms = rtts.get_target_permutations(tt_dfs)
 
 #%%
-perms_ttimes = mu.get_all_ttimes(target_perms, tt_dfs)
+perms_ttimes = rtts.get_all_ttimes(target_perms, tt_dfs)
 
 #%%
-all_ttimes_summary = mu.calculate_total_ttimes(perms_ttimes, target_info)
+all_ttimes_summary = rtts.calculate_total_ttimes(perms_ttimes, target_info)
 
 #%%
-best_routes = mu.get_best_routes(all_ttimes_summary, '', '')
+best_routes = rtts.get_best_routes(all_ttimes_summary, '', '')
 
 #%%
-mu.print_best_route_info(best_routes, target_info)
+rtts.print_best_route_info(best_routes, target_info)
 
 #%%
