23affc7cbde73ad9f1bef462d7fab168bd89b997
Joose Helle <joose.helle@helsinki.fi>
2019-01-11 15:35:01 +0200

    Implement aggregation of adjacent br hubs

--- demo/bike_and_ride_potential.py
+++ demo/bike_and_ride_potential.py
@@ -5,9 +5,9 @@
 #%%
 import pandas as pd
 import geopandas as gpd
-import utils.bike_ride_potential as br_pot
+import utils.bike_ride_potential as br_utils
 from fiona.crs import from_epsg
-from datetime import datetime, timezone, timedelta
+# from shapely.geometry import Point, MultiPoint
 
 #%%
 # read population data
@@ -21,32 +21,29 @@ pop_point['geometry'] = [geom.centroid for geom in pop_point['geometry']]
 pop_point = pop_point.to_crs(from_epsg(4326))
 
 #%%
+# select only points inside AOI (Koskela-polygon)
 point_mask = pop_point.intersects(koskela_poly.loc[0, 'geometry'])
 pop_koskela = pop_point.loc[point_mask]
 
 #%%
-pop_koskela.plot()
-pop_koskela[:5].head(3)
-
-#%%
+# select one point for testing
 test_point = pop_koskela[pop_koskela['INDEX'] == 17420].reset_index()
-test_point.head()
 
 #%% 
-br_dfs = [br_pot.get_bike_ride_effect(row, dest_coords) for index, row in pop_koskela.iterrows()]
+# get and collect all bike & ride (br) routes
+br_dfs = [br_utils.get_bike_ride_effect(row, dest_coords) for index, row in pop_koskela.iterrows()]
 br_df = pd.concat(br_dfs)
-print(br_df.head())
 
 #%%
-cols = ['hsy_idx', 'pop', 'tt', 'tt_diff', 'saved_min', 'b_distance', 'b_duration', 'last_p_str']
+cols = ['hsy_idx', 'pop', 'tt_norm', 'tt_br', 'tt_diff', 'saved_min', 'tt_b', 'dist_b', 'last_p_str']
 br_lines_gdf = gpd.GeoDataFrame(data= br_df[cols], geometry= br_df['arrow'], crs= from_epsg(4326))
 br_lines_gdf.to_file('demo/output/br_lines.shp')
 
 br_routes_gdf = gpd.GeoDataFrame(data= br_df[cols], geometry= br_df['bike_geom'], crs= from_epsg(4326))
 br_routes_gdf.to_file('demo/output/br_routes.shp')
 
-br_homes_gdf = gpd.GeoDataFrame(data= br_df[cols], geometry= br_df['first_point'], crs= from_epsg(4326))
-br_homes_gdf.to_file('demo/output/br_homes.shp')
+br_origins_gdf = gpd.GeoDataFrame(data= br_df[cols], geometry= br_df['first_point'], crs= from_epsg(4326))
+br_origins_gdf.to_file('demo/output/br_origins.shp')
 
 br_hubs_gdf = gpd.GeoDataFrame(data= br_df[cols], geometry= br_df['last_point'], crs= from_epsg(4326))
 br_hubs_gdf.to_file('demo/output/br_hubs.shp')
@@ -54,18 +51,93 @@ br_hubs_gdf.to_file('demo/output/br_hubs.shp')
 #%%
 grouped = br_hubs_gdf.groupby('last_p_str')
 hub_dfs = []
+rownum = 0
 for key, values in grouped:
     sum_pop = values['pop'].sum()
     sum_saved_tt = values['saved_min'].sum()
     avg_saving = sum_saved_tt / sum_pop
-    hsy_idx = list(values['hsy_idx'])[0]
     geom = list(values['geometry'])[0]
-    hub_gdf = gpd.GeoDataFrame(data={'hsy_idx': [hsy_idx], 'sum_pop': [sum_pop], 'avg_saving': [avg_saving]}, geometry= [geom], crs=from_epsg(4326))
+    hub_gdf = gpd.GeoDataFrame(data={'rownum': rownum, 'sum_pop': [sum_pop], 'sum_saved_tt': sum_saved_tt, 'avg_saving': [avg_saving]}, geometry=[geom], crs=from_epsg(4326))
     hub_dfs.append(hub_gdf)
-hub_sum_gdf = pd.concat(hub_dfs)
+    rownum += 1
+hub_sum_gdf = pd.concat(hub_dfs).reset_index(drop=True)
+print(hub_sum_gdf)
 
 #%%
-hub_sum_gdf.to_file('demo/output/br_hubs_sums.shp')
+br_sum_hubs_gdf = br_utils.group_summarize_adjacent_hubs(hub_sum_gdf, 250)
+br_sum_hubs_gdf.to_file('demo/output/br_sum_hubs.shp')
+
+
+
+
+
+
+
+# #%%
+# # project and collect destinations as list
+# hub_sum_gdf['buffer'] = [geom.buffer(150.0) for geom in hub_sum_gdf['geometry']]
+
+# #%%
+# def not_in_any_hub_group(groups, index):
+#     for group in groups:
+#         if index in group:
+#             return False
+#     return True
+
+# # collect aggregated groups of hubs grom adjacent hubs
+# groups = []
+# for idx, row in hub_sum_gdf.iterrows():
+#     group = []
+#     if (not_in_any_hub_group(groups, idx)):
+#         group.append(idx)
+#     buffer = row['buffer']
+#     for compare_idx, compare_row in hub_sum_gdf.iterrows():
+#         if (idx == compare_idx):
+#             print('skipping same point')
+#             continue
+#         if (buffer.intersects(compare_row['buffer'])):
+#             print('found intersecting buffers')
+#             if (not_in_any_hub_group(groups, compare_idx)):
+#                 print('add intersecting hub to same group')
+#                 group.append(compare_idx)
+#             else:
+#                 print('intersecting hub already in a group')
+#     groups.append(group)
+
+# groups = [group for group in groups if len(group)>0]
+# print(groups)
+
+# #%%
+# # add hub new group ids to summarized hubs
+# def get_hub_group(row, groups):
+#     for idx, group in enumerate(groups):
+#         if (row['rownum'] in group):
+#             return idx
+#     return '9999'
+
+# hub_sum_gdf['h_group'] = hub_sum_gdf.apply(lambda row: get_hub_group(row, groups), axis=1)
+# print(hub_sum_gdf)
+
+# # re-summarize hubs based on groups of adjacent hubs
+# #%%
+# grouped = hub_sum_gdf.groupby('h_group')
+# hub_dfs = []
+# rownum = 0
+# for key, values in grouped:
+#     sum_pop = values['sum_pop'].sum()
+#     sum_saved_tt = values['sum_saved_tt'].sum()
+#     avg_saving = sum_saved_tt / sum_pop
+#     geom = list(values['geometry'])[0]
+#     hub_gdf = gpd.GeoDataFrame(data={'rownum': rownum, 'sum_pop': [sum_pop], 'sum_saved_tt': sum_saved_tt, 'avg_saving': [avg_saving]}, geometry=[geom], crs=from_epsg(3879))
+#     hub_dfs.append(hub_gdf)
+#     rownum += 1
+# hub_sum_gdf = pd.concat(hub_dfs).reset_index(drop=True)
+# print(hub_sum_gdf)
+
+
+
+
+
 
 
 
@@ -73,8 +145,8 @@ hub_sum_gdf.to_file('demo/output/br_hubs_sums.shp')
 
 #%%
 #%%
-# projected_hubs = br_hubs_gdf.to_crs(epsg=3879) 
-# dests = list(projected_hubs['geometry'])
+# hub_sum_gdf = br_hubs_gdf.to_crs(epsg=3879) 
+# dests = list(hub_sum_gdf['geometry'])
 # destPoints = MultiPoint(dests)
 # print(destPoints)
 #%
--- utils/bike_ride_potential.py
+++ utils/bike_ride_potential.py
@@ -2,7 +2,9 @@ import utils.dt_routing as dt_rt
 import utils.utils as utils
 import json
 import pandas as pd
+import geopandas as gpd
 from shapely.geometry import Point, LineString, Polygon
+from fiona.crs import from_epsg
 
 def getItinDuration(itin):
     return itin['duration']
@@ -21,15 +23,15 @@ def asMinutes(seconds):
 def get_bike_df(row, population, itin, tt_norm):
     hsy_idx = row['INDEX']
     bike = itin['legs'][0]
-    tt = asMinutes(itin['duration'])
+    tt_br = asMinutes(itin['duration'])
+    tt_b = asMinutes(bike['duration'])
     tt_diff = asMinutes(tt_norm - itin['duration'])
-    b_distance = int(round(bike['distance']))
-    b_duration = asMinutes(bike['duration'])
+    dist_b = int(round(bike['distance']))
     arrow = LineString([bike['first_point'], bike['last_point']])
     saved_min = asMinutes(population * (tt_norm - itin['duration']))
 
-    all_br_data = pd.DataFrame(data={'hsy_idx': [hsy_idx], 'pop': [population], 'tt': [tt], 'tt_diff': [tt_diff], 'saved_min': [saved_min], 'b_distance': [b_distance], 'b_duration': [b_duration], 'first_point': [bike['first_point']], 'last_point': [bike['last_point']], 'last_p_str': [str(bike['last_point'])], 'arrow': [arrow], 'bike_geom': [bike['line_geom']] })
-    return all_br_data
+    br_df = pd.DataFrame(data={'hsy_idx': [hsy_idx], 'pop': [population], 'tt_norm': [tt_norm], 'tt_br': [tt_br], 'tt_diff': [tt_diff], 'saved_min': [saved_min], 'tt_b': [tt_b], 'dist_b': [dist_b], 'first_point': [bike['first_point']], 'last_point': [bike['last_point']], 'last_p_str': [str(bike['last_point'])], 'arrow': [arrow], 'bike_geom': [bike['line_geom']] })
+    return br_df
 
 def get_bike_ride_effect(row, dest_coords):
     population = row['ASUKKAITA']
@@ -53,3 +55,68 @@ def get_bike_ride_effect(row, dest_coords):
         return pd.concat([br_df1, br_df2])
     else:
         return get_bike_df(row, population, itin1, tt_norm)
+
+def group_summarize_adjacent_hubs(hub_sum_gdf, distance):
+    # set radius of the buffer
+    radius = distance/2
+    # reproject
+    hub_sum_gdf = hub_sum_gdf.to_crs(epsg=3879)
+    # reset index
+    hub_sum_gdf= hub_sum_gdf.reset_index(drop=True)
+    # add buffers
+    hub_sum_gdf['buffer'] = [geom.buffer(radius) for geom in hub_sum_gdf['geometry']]
+    
+    # function for determining if hub is already allocated to group or not
+    def not_in_any_hub_group(groups, index):
+        for group in groups:
+            if index in group:
+                return False
+        return True
+
+    # collect groups of adjacent hubs
+    groups = []
+    for idx, row in hub_sum_gdf.iterrows():
+        group = []
+        if (not_in_any_hub_group(groups, idx)):
+            group.append(idx)
+        buffer = row['buffer']
+        for compare_idx, compare_row in hub_sum_gdf.iterrows():
+            if (idx == compare_idx):
+                print('skipping same point')
+                continue
+            if (buffer.intersects(compare_row['buffer'])):
+                print('found intersecting buffers')
+                if (not_in_any_hub_group(groups, compare_idx)):
+                    print('add intersecting hub to same group')
+                    group.append(compare_idx)
+                else:
+                    print('intersecting hub already in a group')
+        groups.append(group)
+
+    groups = [group for group in groups if len(group)>0]
+    print(groups)
+
+    # add group id to a column
+    def get_hub_group(row, groups):
+        for idx, group in enumerate(groups):
+            if (row['rownum'] in group):
+                return idx
+        return '9999'
+    hub_sum_gdf['h_group'] = hub_sum_gdf.apply(lambda row: get_hub_group(row, groups), axis=1)
+
+    # re-summarize hubs based on groups of adjacent hubs
+    grouped = hub_sum_gdf.groupby('h_group')
+    hub_dfs = []
+    rownum = 0
+    for key, values in grouped:
+        sum_pop = values['sum_pop'].sum()
+        sum_saved_tt = values['sum_saved_tt'].sum()
+        avg_saving = sum_saved_tt / sum_pop
+        geom = list(values['geometry'])[0]
+        hub_gdf = gpd.GeoDataFrame(data={'rownum': rownum, 'sum_pop': [sum_pop], 'sum_saved_tt': sum_saved_tt, 'avg_saving': [avg_saving]}, geometry=[geom], crs=from_epsg(3879))
+        hub_dfs.append(hub_gdf)
+        rownum += 1
+    hub_sum_gdf = pd.concat(hub_dfs).reset_index(drop=True)
+    print(hub_sum_gdf)
+
+    return hub_sum_gdf
